9c0b3cd5b6ee4686436feb3a06f4606f86348c6c
Jonathan Land <jonathanland.git@gmail.com>
2021-12-17 00:31:11 -0600

    updates

--- src/main/java/com/jonathanland/controller/MarvelHeroesController.java
+++ src/main/java/com/jonathanland/controller/MarvelHeroesController.java
@@ -10,6 +10,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class MarvelHeroesController {
 
     private MarvelHeroesRepository marvelHeroesRepository;
